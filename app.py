5d342cfcb93cd3187471b10a678cb808f75c1fe3
unknown <ismail.ouali@ump.ac.ma>
2024-05-07 15:28:18 +0000

    Hello world1

---
+++ app.py
@@ -0,0 +1,12 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+
+def plus(n1, n2):
+    result = int(n1) + int(n2)
+
+def hello():
+    print("Hello world!")
+
+print(plus(num1, num2))
+hello()
